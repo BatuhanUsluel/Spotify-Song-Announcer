bb4c7f41230661ebaf0e14d0df9337be3d4a483f
Batuhan Usluel <batuhan.usluel@gmail.com>
2019-07-27 16:06:13 +0300

    Detect file changes

--- Announcer.py
+++ Announcer.py
@@ -1,8 +1,37 @@
-import pyttsx3 
-f = open("Snip.txt", "r", encoding="utf8")
-song = f.read()
-print(song)
+import pyttsx3
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from watchdog.events import PatternMatchingEventHandler
+old = 0
+class MyHandler(PatternMatchingEventHandler):
+    patterns = ["*./Snip.txt"]
+    file_cache = {}
+    def on_modified(self, event):
+        seconds = int(time.time())
+        key = (seconds, event.src_path)
+        if key in self.file_cache:
+            return
+        print ("MODIFIED")
+        self.file_cache[key] = True
+		
+event_handler = MyHandler()
+my_event_handler = PatternMatchingEventHandler()
+observer = Observer()
+observer.schedule(event_handler, path='.', recursive=False)
+observer.start()
 
-engine = pyttsx3.init() 
-engine.say(song) 
-engine.runAndWait() 
+try:
+	while True:
+		time.sleep(1)
+except KeyboardInterrupt:
+	observer.stop()
+observer.join()
+	
+# f = open("Snip.txt", "r", encoding="utf8")
+# song = f.read()
+# print(song)
+
+# engine = pyttsx3.init() 
+# engine.say(song) 
+# engine.runAndWait() 
