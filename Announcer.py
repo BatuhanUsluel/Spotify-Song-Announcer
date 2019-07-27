2456a9865a8d81121c9a72c388e41e8f9197f8a6
Batuhan Usluel <batuhan.usluel@gmail.com>
2019-07-27 21:45:05 +0300

    Fully Functional
    Used the SwSpotify library, made it much more simple.

--- Announcer.py
+++ Announcer.py
@@ -1,37 +1,16 @@
 import pyttsx3
 import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-from watchdog.events import PatternMatchingEventHandler
-old = 0
-class MyHandler(PatternMatchingEventHandler):
-    patterns = ["*./Snip.txt"]
-    file_cache = {}
-    def on_modified(self, event):
-        seconds = int(time.time())
-        key = (seconds, event.src_path)
-        if key in self.file_cache:
-            return
-        print ("MODIFIED")
-        self.file_cache[key] = True
-		
-event_handler = MyHandler()
-my_event_handler = PatternMatchingEventHandler()
-observer = Observer()
-observer.schedule(event_handler, path='.', recursive=False)
-observer.start()
-
-try:
-	while True:
-		time.sleep(1)
-except KeyboardInterrupt:
-	observer.stop()
-observer.join()
-	
-# f = open("Snip.txt", "r", encoding="utf8")
-# song = f.read()
-# print(song)
-
-# engine = pyttsx3.init() 
-# engine.say(song) 
-# engine.runAndWait() 
+from SwSpotify import spotify
+prevsong = ""
+prevartist = ""
+engine = pyttsx3.init() 
+while True:
+    song = spotify.song()
+    artist = spotify.artist()
+    if (not(song == None or artist == None) and (song != prevsong or artist!=prevartist)):
+        engine.say("Playing " + song + " by " + artist)
+        engine.runAndWait()
+        print("Playing " + song + " by " + artist)
+        prevsong = song
+        prevartist = artist
+    time.sleep(1)
